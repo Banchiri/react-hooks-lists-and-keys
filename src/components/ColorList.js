5bd660979256b09b6a26d6059545d4a51c6405be
sandrabanchiri <sandrabanchiri39@gmail.com>
2024-08-21 14:24:47 +0300

    lists and keys

--- src/components/ColorList.js
+++ src/components/ColorList.js
@@ -7,20 +7,41 @@ function ColorList() {
     "salmon",
     "darkslategray",
     "hotpink",
+    "red",
+    "green"
   ];
+  const colorElements = colors.map((color) => {
+    return <ColorItem key={color} color={color} />;
+  });
+
 
   return (
     <div>
-      <h1>Top 5 CSS Colors</h1>
+      <h1>Top 6 CSS Colors</h1>
       <ol>
-        <li style={{ color: colors[0] }}>{colors[0]}</li>
-        <li style={{ color: colors[1] }}>{colors[1]}</li>
-        <li style={{ color: colors[2] }}>{colors[2]}</li>
-        <li style={{ color: colors[3] }}>{colors[3]}</li>
-        <li style={{ color: colors[4] }}>{colors[4]}</li>
+        {colorElements}
+
       </ol>
+      {userHeadings}
+      {fibList}
+      
     </div>
   );
 }
+function ColorItem(props) {
+  return <li style={{color:props.color}}>{props.color}</li>
+}
+const users =[
+  { id:1, firstName:"Duane", lastName:"Watson"},
+  { id:2, firstName:"Duane", lastName:"Johnson"},
+
+];
+const userHeadings = users.map((user) => {
+  return <h1 key={user.id} >{user.firstName}</h1>
+});
+const fib = [0,1,1,2,3,5,7,9];
+const fibList = fib.map((number, index) => {
+  return <div key={index}>{number}</div>
+})
 
 export default ColorList;
